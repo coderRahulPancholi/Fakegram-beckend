acbea790ac131d7e6fbfd028eee1b30d4b37f018
Rahul Sharma <rahulpancholi3937@gmail.com>
2023-04-29 02:24:32 +0530

    logfix

--- Index.js
+++ Index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // app.use(cors())
 app.use(cors({
   credentials:true,
-  origin:"http://localhost:3000"
+  origin:"https://fakegrammern.netlify.app"
   
 }))
 app.use(cookieParser())
